688e7aa463b6b499c51b67605d5ecb0c83403a2e
Aidan Poole <aidanjamie17@gmail.com>
2023-05-02 11:37:55 +0200

    .

--- IWA4/myscripts.js
+++ IWA4/myscripts.js
@@ -1,34 +1,34 @@
 const date = 2050
 const statuses = 'parent' // did not have = but instead : 
-const count = 0
+let count = 0
 
-if (date = 2050) {
+
+if (2050) {
 	console.log("January", 'New Year’s Day') // added bracket 
 	console.log("March", 'Human Rights Day') 
-	date = 'April'
 	console.log(date, 'Family Day')
 	console.log(date, 'Freedom Day')
-    count = count + 4
+    count = count + 4 
 }
-
    
-	if (statuses = 'student') {
+	if (false) {
 	  console.log('June', 'Youth Day')
-		let count = count + 1
+		count = count + 1
   }
 
 
-	if (statuses = "parent") {
+	if ("parent") {
 	console.log(date, 'Christmas Day')
-	let count = count + 1
+	count = count + 1
+	}
+
 	console.log(date, 'Day of Goodwill')
 	count = count + 1
     console.log('August', 'Women’s Day')
 	console.log('September', 'Heritage Day')
-	date = 'December'
 	console.log(date, 'Day of Reconciliation')
 	count = count + 3
-}
+
 
 console.log('Your status is:', statuses)
 console.log('The year is:', date)
