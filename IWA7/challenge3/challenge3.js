f2236343dedd21d4c40d82821d8e519fb91c71c8
Aidan Poole <aidanjamie17@gmail.com>
2023-05-16 10:50:51 +0200

    .

--- IWA7/challenge3/challenge3.js
+++ IWA7/challenge3/challenge3.js
@@ -14,7 +14,7 @@ const sarahnumberBalance = parseInt(sarahBalance)
 const owed = leonumberBalance + sarahnumberBalance
 const leo = leoName+ " " + leoSurname + "(Owed: R" + parseInt(leoBalance) + ")" // incorrect variables used, sarah balance used 
 const sarah = sarahName + sarahSurname + "(Owed: R " +parseInt(sarahBalance).toFixed(2)+ ")" // leoname instead of sarahname used, incorrect variables
-const total = "Total amount owed: R" + owed 
+const total = "Total amount owed: R" + owed.toFixed(2)
 const result = leo + "\n" + sarah + "\n" + divider + "\n" + "\t" + total + "\n" + divider
 
 console.log(result) 
