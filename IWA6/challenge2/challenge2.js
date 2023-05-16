ffda4e0f68a4a8491d38cdd471d150cf17063b30
Aidan Poole <aidanjamie17@gmail.com>
2023-05-16 10:14:46 +0200

    .

--- IWA6/challenge2/challenge2.js
+++ IWA6/challenge2/challenge2.js
@@ -1,5 +1,5 @@
 const rent = 400;
-const tax = 0.08 ;
+const tax = 8 ;
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
@@ -9,7 +9,7 @@ const minuteOfDay = 00;
 // Only change below this line
 
 if (hourOfDay && minuteOfDay !== null && hourOfDay == '00' && minuteOfDay == '00') {
-	const taxAsDecimal = tax / 100
+	const taxAsDecimal = parseInt(tax / 100)
 	const startingAfterTax = salary * 1 - taxAsDecimal
 	const balance = startingAfterTax - transport - food - rent
 }
