3cc9694eb9458d80be3f7af57d260b181f79d27e
Aidan Poole <aidanjamie17@gmail.com>
2023-05-23 13:37:38 +0200

    .

---
+++ IWA11/challenge1.js
@@ -0,0 +1,38 @@
+// script.js
+
+const rootOne = document.querySelector('[data-key="order1"]');
+const biscuitsOne= document.querySelector('[data-biscuits="10"]');
+const donutsOne= document.querySelector('[data-donuts="13"]');
+const pancakesOne= document.querySelector('[data-pancakes="0" ]');
+const statusOne= document.querySelector('[data-delivered="false"]');
+
+
+const rootTwo = document.querySelector('[data-key="order2"]');
+const biscuitsTwo = document.querySelector('[data-biscuits="5"]');
+const donutsTwo = document.querySelector('[data-donuts="0"]');
+const pancakesTwo= document.querySelector('[data-pancakes="2"]');
+const statusTwo= document.querySelector('[data-delivered="false"]')
+
+
+const rootThree = document.querySelector('[data-key="order3"]');
+const biscuitsThree= document.querySelector('[data-biscuits="12"]');
+const donutsThree= document.querySelector('[data-donuts="11"]');
+const pancakesThree= document.querySelector('[data-pancakes="15"]');
+const statusThree= document.querySelector('[data-delivered="true"]');
+
+data-biscuitsOne = data.biscuits;
+data-donutsOne = data-root.donuts;
+data-pancakesOne = data-root.pancakes;
+data-statusOne = data-root.status ? Delivered = Pending:
+
+
+
+biscuits= root.biscuits;
+donuts = root.donuts;
+pancakes = root.pancakes;
+statuses = root.status ? Delivered = Pending:
+
+biscuits = root.biscuits;
+donuts = root.donuts;
+pancakes = root.pancakes;
+statuses = root.status ? Delivered = Pending: 
