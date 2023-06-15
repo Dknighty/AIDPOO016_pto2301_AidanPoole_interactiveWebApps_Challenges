c3bc8acc2592339741d4ddbb4c2cb0cd985cff4d
Aidan Poole <aidanjamie17@gmail.com>
2023-06-15 12:57:14 +0200

    .

---
+++ IWA14/challenge2/challenge2.js
@@ -0,0 +1,62 @@
+// script.js
+
+const add = () => {
+    a + b
+}
+
+const multiply = () => {
+    a * b 
+}
+
+function internal() {
+	const added = this.add(this.a, this.b)
+	multiply(this.a, this.b)
+	return this
+}
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+example1.calculate()
+example2.calculate()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(48)
+console.log(12)
